c62cf967f1aaf0e073fbb37a8f79372646bb6969
KevHLJob <KevHLJob@github.com>
2023-07-20 14:20:35 -0600

    add meta and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" title='Wanner Servicios'>
+      <meta name='Wanner Servicios'
+      content='Servicios de Ingeniería electromecánica'/>
       <body>
         <ChakraProvider>
           <Navbar />
